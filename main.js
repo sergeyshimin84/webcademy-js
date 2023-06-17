c4bbc2024ac881701ccc144a621e7cd1cd4d978b
Sergey Shimin <sergey.shimin84@gmail.com>
2023-06-17 20:43:20 +0700

    6.1 Параметры и аргументы для функций 6.2 return

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 'use strict';
 
-// ------------------ 6. Функции ----------------
+// ------------------ 6.1 Параметры и аргументы для функций ----------------
 
-sayHi();
-seyHi();
-// function decloration
-function sayHi() {
-    console.log('Привет!');
+// const userName = 'Марк';
+// console.log(`Привет, ${userName}! Как твои дела`);
+
+function sayHi(name) {
+    console.log(`Привет, ${name}! Как твои дела`);
 }
 
-// function expression
-const seyHi = function () {
-    console.log('Привет!');
+sayHi('Марк');
+sayHi('Олег');
+sayHi('Женя');
+
+// ------------------- 6.2 return -----------------------
+function summ(a, b) {
+    const result = a + b;
+    return result;//после return функция завершает работу
 }
 
+let res = summ(15, 20);
+console.log(res);

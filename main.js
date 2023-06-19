2feb52481d84165fa6c856b47a31c64bc37675bb
Sergey Shimin <sergey.shimin84@gmail.com>
2023-06-19 16:43:37 +0700

    8. Циклы 9. Обход массива иклом for

--- main.js
+++ main.js
@@ -1,30 +1,19 @@
 'use strict';
 
-// ------------------ 7.1 Методы массивов ----------------
+// ------------------ 8. Циклы ----------------
+console.log('Start');
 
-/*
-arr.push(items) - Добавить элемент в конец массива
-arr.pop() - Удалить элемент из конца массива
-arr.shift() - Удалить элемент из начала массива
-arr/unshift(...items) - Добавить элемент в начало массива
-arr.splice([start]), [deketeCount, newElements])
-*/
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
 
-const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
-console.log(autoBrands);
-/*
-Можем работать с данным массивом в переменной объявленной const,
-т.к. массивы и объекты записываються в переменную не целиком, 
-а переменная как бы ссылаеться на этот массив/объект 
-(если данная константа сошлется на новый массив, тогда получим ошибку)
-*/
-autoBrands.push('Nissan'); 
-console.log(autoBrands);
-// array2 == autoBrands будет true, т.к. оба массива ссылаются на одну и туже область памяти
-let array2 = autoBrands;
-// array3 == autoBrands будет false, т.к. массивы ссылаются на разные области памяти, несмотря на идентичность содержимого
-let array3 = ['Audi', 'BMW', 'Mazda', 'Toyota'];
+console.log('Finish');
 
-// Удаляем один элемен из массива со второй позиции (включительно)
-autoBrands.splice(2, 1);
-console.log(autoBrands);
+// ------------------ 9. Обход массива иклом for ----------------
+
+const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota', 'Nissan']; 
+console.log(autoBrands.length);
+
+for (let i = 0; i < autoBrands.length; i++) {
+    console.log(autoBrands[i]);
+}

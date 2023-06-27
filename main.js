2ac7a10808deea04bfa88834bd32a85288f5f1b2
Sergey Shimin <sergey.shimin84@gmail.com>
2023-06-27 18:16:10 +0700

    Выбор DOM элементов

--- main.js
+++ main.js
@@ -8,3 +8,31 @@
 
 const header = document.querySelector('h2');
 console.log(header);
+
+header.classList.add('red');
+
+document.querySelector('.heading-3').classList.add('green');
+
+// Выбор коллекции элементов
+const headings = document.querySelectorAll('h2');
+console.log(headings);
+// Нашшли все заголовки, пробижаллись циклом for of, добавили к каждому класс .red-text
+for (let heading of headings) {
+    console.log(heading);
+    heading.classList.add('red-text');
+}
+// Нашшли все заголовки, пробижаллись циклом for of, добавили к каждому класс .green-text
+const paragraphs = document.querySelectorAll('p');
+for (let p of paragraphs) {
+    console.log(p);
+    p.classList.add('green-text');
+}
+// Пример с forEach
+paragraphs.forEach(function (item) {
+    item.classList.add('green-text');
+})
+
+// Также есть следующие методы
+document.getElementsByClassName();// выбор по имени класса
+document.getElementsByTagName();// выбор по тегу
+document.getElementById('');// выбор по id

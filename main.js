8c10f70e5b3d5b3263e413b4c55a0cf14f30f8f2
Sergey Shimin <sergey.shimin84@gmail.com>
2023-06-28 16:12:28 +0700

    Работа с CSS классами

--- main.js
+++ main.js
@@ -1,38 +1,20 @@
 'use strict';
 
-// ------------------ Выбор DOM элементов ----------------
+// ------------------ Работа с CSS классами ----------------
 
-// Выбор одного элеменнта DOM по селектору
-// document.querySelector('selector'); 
-// Обращаемся к глобальному объекту браузера document (querySelector выбирает первый элемент на странице, поиск происходит сверху вниз)
+/* 
+element.classList.add()
+.add() добавляет класс
+.remove() удаляет класс
+.toggle() добавляет или удаляет класс при его наличии ("переключатель"). 
+Когда toggle добавляет класс, возвращает true, когда убиррает false.
+.contaiins() проверяет наличие класса у элемента. Возвращает true или false.
+*/
 
-const header = document.querySelector('h2');
-console.log(header);
+const heading = document.querySelector('h2');
+console.log(heading);
 
-header.classList.add('red');
-
-document.querySelector('.heading-3').classList.add('green');
-
-// Выбор коллекции элементов
-const headings = document.querySelectorAll('h2');
-console.log(headings);
-// Нашшли все заголовки, пробижаллись циклом for of, добавили к каждому класс .red-text
-for (let heading of headings) {
-    console.log(heading);
-    heading.classList.add('red-text');
-}
-// Нашшли все заголовки, пробижаллись циклом for of, добавили к каждому класс .green-text
-const paragraphs = document.querySelectorAll('p');
-for (let p of paragraphs) {
-    console.log(p);
-    p.classList.add('green-text');
-}
-// Пример с forEach
-paragraphs.forEach(function (item) {
-    item.classList.add('green-text');
-})
-
-// Также есть следующие методы
-document.getElementsByClassName();// выбор по имени класса
-document.getElementsByTagName();// выбор по тегу
-document.getElementById('');// выбор по id
+heading.classList.add('red-text');
+heading.classList.remove('red-text');
+heading.classList.toggle('green-text');
+heading.classList.toggle('green');

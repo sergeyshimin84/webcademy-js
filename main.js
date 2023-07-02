7c06c356f5f903caa83e27394b4c7a0909e2f63c
Sergey Shimin <sergey.shimin84@gmail.com>
2023-07-02 14:08:25 +0700

    Работа с элементами

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
 'use strict';
 
-// ------------------ Объект event ----------------
+// ------------------ Работа с элементами ----------------
 
-const list = document.querySelector('#list');
-// Объект event являетс первым параметром функции addEventListener
-list.addEventListener('click', (event) => {
-    // this ссылаеться на родителя с #list (у которого слушаем addEventListener)
-    console.log(this);
-    // Объект event получает всю информацию о html элементе спровоцировавшим событие ('click') 
-    console.log(event);
-    // Свойство target получает информацию о том html элементе который спровоцировал событие ('click') 
-    console.log(event.target);
-})
+/*
+// Создать элемент
+document.createElement('tag-name')
+
+// Изменить HTML содержимое внутри элемента
+element.innerHTML = ''
+
+// Изменить текстовое содержимое внутри элемента
+none.innerText = ''
+
+// Клонирование элемента
+node.cloneNode() // true с внутренним содержанием (текста и теги),
+false - без внутреннего содержания
+
+// Вставить элемент внутрь другого элемента
+element.append(nodesOrDOMStrings)
+
+// Удалить элемент
+element.remove()
+*/
+
+// Выбор контейнера
+const container = document.querySelector('#elementsContainer');
+
+// Создание загловка
+const newHeader = document.createElement('h1');
+newHeader.innerText = 'Новый заголовок';
+container.append(newHeader);
+
+// Копирование шапки
+const mainHeader = document.querySelector('header');
+// Для копирования всего содержимого тега в метод cloneNode передаем true 
+const headerCopy = mainHeader.cloneNode(true);
+// Вставляем элемент в container
+container.append(headerCopy);
+
+// Вставка разметки через строку
+const htmlExample = '<h2>Еще один заголовок</h2>';
+container.insertAdjacentHTML('beforeend', htmlExample);
+
+// Вставка разметки через шаблонные строки
+const title = 'Текст заголовка';
+// Передаем текст через шаблонную строку
+const htmlExample2 = `<h2>${title}</h2>`;
+container.insertAdjacentHTML('beforeend', htmlExample2);
